d939a0f8ed60f1b65cc722dd06186996691f5914
Wander Rodrigues <wander.s.rodrigues@gmail.com>
2020-09-16 13:20:19 -0300

    Add files via upload

---
+++ Walkthrough/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+		onShowHello: function () {
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog: function () {
+			this.getOwnerComponent().openHelloDialog();
+		}
+
+	});
+});
